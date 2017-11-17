23e8718252d5649d9fc16e0801ca8374a596c2aa
Ben Roth <spongebucket@gmail.com>
2017-11-17 09:50:02 -0800

    generate terrain better

--- entities/Terrain.js
+++ entities/Terrain.js
@@ -2,14 +2,15 @@
 import * as THREE from 'three';
 import GameState from '../state/GameState';
 
-const TERRAIN_NUM_SEGMENTS = 5;
+const TERRAIN_NUM_SPANS = 12;
 const TERRAIN_NEUTRAL_Y = 0;
 
 export default class Terrain {
   constructor() {
     const scene = GameState.scene;
+    this._spans = this._generateTerrain(TERRAIN_NEUTRAL_Y);
     this._material = new THREE.MeshBasicMaterial({ color: 0xe28631 }),
-    this._mesh = new THREE.Mesh(this._makeShapeGeometry(), this._material);
+    this._mesh = new THREE.Mesh(this._makeShapeGeometry(this._spans), this._material);
     scene.add(this._mesh);
   }
 
@@ -18,7 +19,7 @@ export default class Terrain {
     scene.remove(this._mesh);
   }
 
-  _makeShapeGeometry = () => {
+  _makeShapeGeometry = (spans) => {
     const viewport = GameState.viewport;
     let width = viewport.width;
     let shape = new THREE.Shape();
@@ -26,9 +27,11 @@ export default class Terrain {
 
     // (interpolating, 1 + value)
     const depths = [ 0.1, -0.1, 0.05, -0.05, 0.2 ];
-    for (let ii = 0; ii < TERRAIN_NUM_SEGMENTS; ii++) {
-      let xInterp = ii / (TERRAIN_NUM_SEGMENTS - 1.0);
-      shape.lineTo(-(viewport.width / 2) + (xInterp * width), TERRAIN_NEUTRAL_Y + depths[ii]);
+    for (let ii = 0; ii < TERRAIN_NUM_SPANS; ii++) {
+      let xInterpLeft = ii / (TERRAIN_NUM_SPANS);
+      let xInterpRight = (ii + 1) / (TERRAIN_NUM_SPANS);
+      shape.lineTo(-(viewport.width / 2) + (xInterpLeft * width), TERRAIN_NEUTRAL_Y + spans[ii][0]);
+      shape.lineTo(-(viewport.width / 2) + (xInterpRight * width), TERRAIN_NEUTRAL_Y + spans[ii][1]);
     }
     // neutral top-right corner
     shape.lineTo(viewport.width / 2, TERRAIN_NEUTRAL_Y);
@@ -37,5 +40,23 @@ export default class Terrain {
     shape.lineTo(viewport.width / 2, -viewport.height / 2);
     shape.lineTo(-viewport.width / 2, -viewport.height / 2);
     return new THREE.ShapeGeometry(shape);
+  }
+
+  _generateTerrain = (startY) => {
+    let spans = [];
+    let prevY = startY;
+    for (let ii = 0; ii < TERRAIN_NUM_SPANS; ii++) {
+      const isDiscontinuous = (Math.random() < 0.2);
+      if (isDiscontinuous) {
+        prevY = TERRAIN_NEUTRAL_Y - 0.1 + Math.random() * 0.2;
+      }
+      const span = [
+        prevY,
+        prevY - 0.1 + Math.random() * 0.2,
+      ];
+      prevY = span[1];
+      spans.push(span);
+    }
+    return spans;
   }
 }
