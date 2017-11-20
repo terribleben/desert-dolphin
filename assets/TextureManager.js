56ac43c384298c5cc8ef61f41d4ce88cb5e7b9af
Ben Roth <spongebucket@gmail.com>
2017-11-19 18:55:44 -0800

    some texture improvements

--- assets/TextureManager.js
+++ assets/TextureManager.js
@@ -1,13 +1,17 @@
 import Expo from 'expo';
 import ExpoTHREE from 'expo-three';
+import * as THREE from 'three';
 
 class TextureManager {
   DOLPHIN = 'dolphin';
   GUIDE = 'guide';
   SCOPE = 'scope';
+  LOSER = 'loser';
   
   loadAsync = async () => {
     this._textures = {};
+    this._materials = {};
+    
     this._textures[this.DOLPHIN] = await ExpoTHREE.createTextureAsync({
       asset: Expo.Asset.fromModule(require('../assets/dolphin.png')),
     });
@@ -17,11 +21,20 @@ class TextureManager {
     this._textures[this.SCOPE] = await ExpoTHREE.createTextureAsync({
       asset: Expo.Asset.fromModule(require('../assets/scope.png')),
     });
+
+    this._materials[this.LOSER] = new THREE.MeshBasicMaterial({
+      map: this.get(this.DOLPHIN),
+      transparent: true,
+    });
     return;
   }
 
   get = (id) => {
     return this._textures[id];
+  }
+
+  getMaterial = (id) => {
+    return this._materials[id];
   }
 }
 
--- entities/Loser.js
+++ entities/Loser.js
@@ -4,26 +4,18 @@ import TextureManager from '../assets/TextureManager';
 
 export default class Loser {
   constructor(position, rotation) {
-    this._buildMeshAsync(position, rotation);
-  }
-
-  updateXPosition = (x) => {
-    this._mesh.position.x += x;
-  }
-
-  _buildMeshAsync = async (position, rotation) => {
     const scene = GameState.scene;
     const geometry = new THREE.PlaneBufferGeometry(0.15, 0.15);
-    this._material = new THREE.MeshBasicMaterial({
-      map: TextureManager.get(TextureManager.DOLPHIN),
-      transparent: true,
-    });
-    this._mesh = new THREE.Mesh(geometry, this._material);
+    this._mesh = new THREE.Mesh(geometry, TextureManager.getMaterial(TextureManager.LOSER));
     scene.add(this._mesh);
     this._mesh.position.x = position.x;
     this._mesh.position.y = position.y;
     this._mesh.position.z = -0.5;
     this._mesh.rotation.z = rotation;
+  }
+
+  updateXPosition = (x) => {
+    this._mesh.position.x += x;
   }
 
   destroy = () => {
